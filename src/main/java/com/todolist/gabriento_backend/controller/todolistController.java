c76ee84dffb3c367935eeb3bca3e09e1edae70b6
anna <anniegabriento51@gmail.com>
2025-06-16 17:06:48 +0800

    add cors

--- src/main/java/com/todolist/gabriento_backend/controller/todolistController.java
+++ src/main/java/com/todolist/gabriento_backend/controller/todolistController.java
@@ -2,6 +2,7 @@ package com.todolist.gabriento_backend.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.todolist.gabriento_backend.entity.todolist;
 import com.todolist.gabriento_backend.service.todolistService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class todolistController {
 
